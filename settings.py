2e4eb174f2955e07b19dabd4b5321c8f724cfac7
KingWilliamsGPT <williamusanga23@gmail.com>
2024-09-07 21:27:55 +0100

    updated_doc

--- settings.py
+++ settings.py
@@ -21,7 +21,6 @@ class Settings:
     DEBUG: bool = env.bool("DEBUG", False)
     LOG_LEVEL: int = INFO if DEBUG else WARNING
 
-    BOT_TOKEN: str = env.str("TELEGRAM_TOKEN")
     SECRET_TOKEN: str = env.str("SECRET_TOKEN")
      
     BOT_WEB_URL: str = env.str("BOT_WEB_URL")
